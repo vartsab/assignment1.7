242bc6fe63bf6f63e5424d0427d02bcc558b01b1
example <dan>
2022-01-18 18:00:38 -0500

    1st attempt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //#1
+        System.out.println("Задача №1");
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName + "." );
+
+        //#2
+        System.out.println("\nЗадача №2");
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //#3
+        System.out.println("\nЗадача №3");
+
+        System.out.println("Данные ФИО сотрудника для административного отдела — " + fullName.replace(" ", "; "));
+
+        //#3
+        System.out.println("\nЗадача №3");
+
+        fullName = "Иванов Семён Семёнович";
+
+        if ( fullName.contains("ё") )
+            fullName = fullName.replace('ё', 'е');
+
+        System.out.println("Данные ФИО сотрудника — " + fullName + ".");
     }
 }
